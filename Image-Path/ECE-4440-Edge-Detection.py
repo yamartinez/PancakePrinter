b3f5b246610dadf7b0306acec3e9a86c1ce4c23d
Devin Gardner <deg9hc@virginia.edu>
2021-11-30 13:16:40 -0500

    Edge Detection and Color Detection have been constructed and tested to perform well on satisfactory images, still need to work on advanced photographs

--- Image-Path/ECE-4440-Edge-Detection.py
+++ Image-Path/ECE-4440-Edge-Detection.py
@@ -3,13 +3,13 @@
 
 import sys
 import cv2
+import numpy as np
 
 # Read the original image
 name = sys.argv[1]
 img_path = 'Edge-Detection-Test-' + name + '.jpg'
-try:
-    img = cv2.imread(img_path)
-except OSError:
+img = cv2.imread(img_path)
+if (img is None):
     print("Could not read file: " + img_path)
     exit(1)
 
@@ -31,18 +31,55 @@ img_resize = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 cv2.imshow('Original', img_resize)
 cv2.waitKey(0)
 
-# TODO: Contrast colors in image for better edge detection
+"""
+# NOTE: Do we need contrast? It does improve the edge detection for more complex images... 
+# Leaving contrast here for now in case we decide we want it...
+contrast = 64
+alpha = 131*(contrast + 127)/(127*(131-contrast))
+gamma = 127*(1-alpha)
+img_contrast = cv2.addWeighted(img_resize, alpha, img_resize, 0, gamma)
+
+# TESTING
+cv2.imshow('Contrast', img_contrast)
+cv2.waitKey(0)
+"""
 
 # Convert to graycsale
 img_gray = cv2.cvtColor(img_resize, cv2.COLOR_BGR2GRAY)
+# Normalize image
+img_normalize = np.zeros(img_gray.shape[:2])
+img_normalize = cv2.normalize(img_gray, img_normalize, 0, 255, cv2.NORM_MINMAX)
+
 # Blur the image for better edge detection
-img_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
-# TODO: Apply background erosion to remove unnecessary background edges
+img_blur = cv2.GaussianBlur(img_normalize, (3,3), 0)
 
 # Canny Edge Detection
 edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200) # Canny Edge Detection
 # Display Canny Edge Detection Image
 cv2.imshow('Canny Edge Detection', edges) # edges is a binary mask of the detected edges
 cv2.waitKey(0)
- 
+
+# Dilate image to connect loose ends of edges
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+img_morph = cv2.morphologyEx(edges, cv2.MORPH_DILATE, kernel)
+cv2.imshow('With Morphological Transformation', img_morph)
+cv2.waitKey(0)
+
+"""
+# Background erosion is a work in progress, keeping it in case we want it...
+# IDEA: Don't find contours, just use edges to find contour area?
+img_contours = cv2.findContours(img_morph, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
+img_contours = sorted(img_contours, key=cv2.contourArea)
+for i in img_contours:
+    if cv2.contourArea(i) > 9000:
+        break
+mask = np.zeros(img_morph.shape[:2], np.uint8)
+cv2.drawContours(mask, [i], -1, 255, -1)
+cv2.imshow('Contours', mask)
+cv2.waitKey(0)
+edges = cv2.bitwise_and(edges, mask)
+cv2.imshow('Background Erosion', edges) # edges is a binary mask of the detected edges
+cv2.waitKey(0)
+"""
+
 cv2.destroyAllWindows()
